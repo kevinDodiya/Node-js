78d0d65a19bf0415037d94eec1705de0fdc4d08e
kevinDodiya <kevindodiya75@gmail.com>
2024-06-02 11:41:32 +0530

    start node js learn

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer();
+
+const friends = [
+  {
+    id: 0,
+    name: 'Nikola Tesla',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+  {
+    id: 2,
+    name: 'Albert Einstein',
+  }
+];
